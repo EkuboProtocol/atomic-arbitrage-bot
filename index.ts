5591dc5abd1c168869824b4c270609200e3d33cd
Moody Salem <moody.salem@gmail.com>
2024-04-04 13:50:33 -0400

    start populating the calls

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import "dotenv/config";
-import { RpcProvider } from "starknet";
+import { Call, Contract, RpcProvider } from "starknet";
+import ROUTER_ABI from "./router-abi.json";
 
 const EKUBO_API_QUOTE_URL = process.env.EKUBO_API_QUOTE_URL;
-const TOKEN_TO_ARBITRAGE = process.env.TOKEN_TO_ARBITRAGE;
+const TOKEN_TO_ARBITRAGE = process.env.TOKEN_TO_ARBITRAGE!;
 const MAX_HOPS = Math.max(2, Number(process.env.MAX_HOPS));
 const MAX_SPLITS = Math.max(2, Number(process.env.MAX_SPLITS));
 const CHECK_INTERVAL_MS = Math.max(3000, Number(process.env.CHECK_INTERVAL_MS));
@@ -11,12 +12,22 @@ const MAX_POWER_OF_2 = Math.max(
   MIN_POWER_OF_2 + 1,
   Math.min(65, Number(process.env.MAX_POWER_OF_2))
 );
+const NUM_TOP_QUOTES_TO_ESTIMATE = Math.max(
+  1,
+  Number(process.env.NUM_TOP_QUOTES_TO_ESTIMATE)
+);
 
 const JSON_RPC_URL = process.env.JSON_RPC_URL;
 
-const rpcProvider = new RpcProvider({
+const RPC_PROVIDER = new RpcProvider({
   nodeUrl: JSON_RPC_URL,
 });
+
+const ROUTER_CONTRACT = new Contract(
+  ROUTER_ABI,
+  process.env.ROUTER_ADDRESS!,
+  RPC_PROVIDER
+);
 
 export interface ApiQuoteSwap {
   specifiedAmount: string;
@@ -69,7 +80,7 @@ console.log("Starting with config", {
 
 (async function () {
   while (true) {
-    const top5ArbitrageResults = (
+    const topArbitrageResults = (
       await Promise.all(
         AMOUNTS_TO_QUOTE.map(async (amount) => {
           const quote = await fetchQuote(amount);
@@ -86,18 +97,58 @@ console.log("Starting with config", {
         })
       )
     )
-      // filters to the profitable quotes
+      // filters to the on-paper profitable quotes (not accounting for gas)
       .filter((quote): quote is Exclude<typeof quote, null> =>
         Boolean(quote && quote.profit > 0n)
       )
       .sort(({ profit: profitA }, { profit: profitB }) =>
         Number(profitB - profitA)
       )
-      .slice(0, 5);
+      .slice(0, NUM_TOP_QUOTES_TO_ESTIMATE)
+      .map((result): typeof result & { calls: Call[] } => {
+        const {
+          amount,
+          quote: { total, splits },
+        } = result;
 
-    console.log("Top 5", top5ArbitrageResults);
+        if (splits.length === 0) {
+          throw new Error("unexpected number of splits");
+        }
 
-    // todo: execute
+        let slippageAdjustedTotal = (BigInt(total) * 99_990n) / 10_000n;
+        if (slippageAdjustedTotal < amount) {
+          slippageAdjustedTotal = amount;
+        }
+
+        if (splits.length === 1) {
+          const split = splits[0];
+          if (split.route.length === 1) {
+            throw new Error("unexpected single hop route");
+          } else {
+            return {
+              ...result,
+              calls: [
+                ROUTER_CONTRACT.populate("clear_minimum", [
+                  { contract_address: TOKEN_TO_ARBITRAGE },
+                  slippageAdjustedTotal,
+                ]),
+              ],
+            };
+          }
+        }
+
+        return {
+          ...result,
+          calls: [
+            ROUTER_CONTRACT.populate("clear_minimum", [
+              { contract_address: TOKEN_TO_ARBITRAGE },
+              slippageAdjustedTotal,
+            ]),
+          ],
+        };
+      });
+
+    console.log(`Top ${NUM_TOP_QUOTES_TO_ESTIMATE}`, topArbitrageResults);
 
     await sleep(CHECK_INTERVAL_MS);
   }
